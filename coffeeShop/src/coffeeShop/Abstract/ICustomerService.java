8892517ea6169e41f342ab4177bc56b35992440d
Utku Yıldız <utkuy166@gmail.com>
2021-06-06 01:33:30 +0300

    HomeWork-4

--- coffeeShop/src/coffeeShop/Abstract/ICustomerService.java
+++
@@ -1,9 +0,0 @@
-package coffeeShop.Abstract;
-
-import coffeeShop.Concrete.Entities.Customer;
-
-public interface ICustomerService {
-	
-	void Save(Customer customer);
-
-}
--- coffeeShop/src/coffeeShop/Abstract/IEntitie.java
+++
@@ -1,5 +0,0 @@
-package coffeeShop.Abstract;
-
-public interface IEntitie {
-
-}
